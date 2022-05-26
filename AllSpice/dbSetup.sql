7933e9298389f041445d5533bd82415a103c30f2
Maria Walker <mariawalker3232@gmail.com>
2022-05-26 15:08:54 -0600

    initial

---
+++ AllSpice/dbSetup.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS accounts(
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
+) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS recipes(
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    picture TEXT NOT NULL,
+    title TEXT NOT NULL,
+    subtitle TEXT NOT NULL,
+    category TEXT NOT NULL,
+    creatorId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8;
+
+CREATE TABLE IF NOT EXISTS ingredients(
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    name TEXT NOT NULL,
+    quantity TEXT NOT NULL,
+    recipeId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8;
+
+CREATE TABLE IF NOT EXISTS steps(
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    position TEXT NOT NULL,
+    body TEXT NOT NULL,
+    recipeId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8;
+
+CREATE TABLE IF NOT EXISTS favorites(
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    recipeId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE,
+    accountId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8;
